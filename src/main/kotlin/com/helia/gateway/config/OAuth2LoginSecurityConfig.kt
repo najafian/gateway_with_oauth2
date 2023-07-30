f5eb85b8347e386cddd78cce608743c600119d03
Mehdi Najafian <najafian@live.com>
2023-07-30 12:03:41 +0200

    added oauth2

---
+++ src/main/kotlin/com/helia/gateway/config/OAuth2LoginSecurityConfig.kt
@@ -0,0 +1,24 @@
+package com.helia.gateway.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity.AuthorizeExchangeSpec
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+@Configuration(proxyBeanMethods = false)
+@EnableWebFluxSecurity
+class OAuth2LoginSecurityConfig {
+    @Bean
+    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        http
+            .authorizeExchange { authorize: AuthorizeExchangeSpec ->
+                authorize
+                    .anyExchange().authenticated()
+            }
+            .oauth2Login(Customizer.withDefaults())
+        return http.build()
+    }
+}
